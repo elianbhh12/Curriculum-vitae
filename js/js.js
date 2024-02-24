5b4766977380c078fd5420eebfd1cadd603f1a5c
elianbhh12 <elianbhhhgmail.com>
2024-02-23 22:02:32 -0500

    Primera

---
+++ js/js.js
@@ -0,0 +1,29 @@
+let elemento;
+
+elemento = document;
+elemento1 = document.head;
+elemento2 = document.body;
+
+elemento3 = document.URL;
+elemento4 = document.characterSet;
+elemento5 = document.scripts;
+elemento6 = document.images;
+elemento7 = document.contentType;
+
+elemento8 = document.links;
+elemento9 = document.links[3];
+
+
+
+
+console.log(elemento)
+console.log(elemento1)
+console.log(elemento2)
+console.log(elemento3)
+console.log(elemento4)
+console.log(elemento5)
+console.log(elemento6)
+console.log(elemento7)
+console.log(elemento8)
+console.log(elemento9)
+
